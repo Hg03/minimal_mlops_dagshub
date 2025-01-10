41ddcf967ef0cd1fa6edeb468dda12e8d3c98236
Hg03 <gehloth03@gmail.com>
2025-01-10 09:30:52 +0000

    feature engineer pipeline completed baseline

--- minimal_mlops/src/feature_pipeline/feature_engineer_utils.py
+++ minimal_mlops/src/feature_pipeline/feature_engineer_utils.py
@@ -2,9 +2,15 @@ from dotenv import load_dotenv
 from supabase import create_client
 import os
 import polars as pl
+import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, FunctionTransformer
+from sklearn.compose import make_column_transformer
+from sklearn.pipeline import make_pipeline
 from pathlib import Path
 from typing import Dict
+import numpy as np
 from tqdm import tqdm
 
 def load_data_from_supabase(config: Dict, raw_data_path: Path) -> pl.DataFrame:
@@ -35,5 +41,47 @@ def split_data(config: Dict, raw_data: pl.DataFrame) -> None:
     test.write_parquet(Path(os.path.join(config["path"]["root"], config["path"]["data"], config["file"]["raw_testing_data"])))
     
 
+def load_raw_from_local(config: Dict) -> Dict[str, pl.DataFrame]:
+    return {
+        "train": pl.read_parquet(Path(os.path.join(config["path"]["root"], config["path"]["data"], config["file"]["raw_training_data"]))),
+        "test": pl.read_parquet(Path(os.path.join(config["path"]["root"], config["path"]["data"], config["file"]["raw_testing_data"]))),
+    }
+
+
+def column_preserver(data: pl.DataFrame):
+    original_columns = [col[col.rfind('__') + 2:] for col in data.columns]
+    data = data.rename(dict(zip(data.columns, original_columns)))
+    return data
+
+def transformation_pipeline(config: Dict):  
+    numerical_imputer = SimpleImputer(strategy=config["preprocessing_strategies"]["numerical_impute"])
+    categorical_imputer = SimpleImputer(strategy=config["preprocessing_strategies"]["categorical_impute"])
+    keep_the_name_same = FunctionTransformer(column_preserver)
+    imputation_transformer = make_column_transformer(
+        (numerical_imputer, config["columns"]["numeric"]),
+        (categorical_imputer, config["columns"]["categoric"]),
+    remainder='passthrough').set_output(transform='polars')
+    pipe_I =   make_pipeline(imputation_transformer, keep_the_name_same)
+    encoding_transformer = make_column_transformer(
+        (OneHotEncoder(sparse_output=False, handle_unknown='ignore'), config["columns"]["nominal"]),
+        (OrdinalEncoder(handle_unknown="use_encoded_value", unknown_value=-1), config["columns"]["ordinal"]),
+        remainder='passthrough'
+    ).set_output(transform='polars')
+    pipe_II = make_pipeline(encoding_transformer, keep_the_name_same)
+    
+    final_pipe = make_pipeline(pipe_I, pipe_II)
+    return final_pipe
+    
+
 def preprocess(config: Dict) -> Dict[str, pl.DataFrame]:
-    pass
+    raw = load_raw_from_local(config=config)
+    raw_train, raw_test = raw["train"], raw["test"]
+    preprocessor = transformation_pipeline(config=config)
+    preprocessed_train = preprocessor.fit_transform(raw_train)
+    preprocessed_test = preprocessor.transform(raw_test)
+    preprocessed_train.write_parquet(Path(os.path.join(config["path"]["root"], config["path"]["data"], config["file"]["processed_training_data"])))
+    preprocessed_test.write_parquet(Path(os.path.join(config["path"]["root"], config["path"]["data"], config["file"]["processed_testing_data"])))
+    return {
+        "train": preprocessed_train,
+        "test": preprocessed_test
+    }
