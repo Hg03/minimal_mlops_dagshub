b4dd73ca421356d2114bf1daf73303e53fb8f882
Hg03 <gehloth03@gmail.com>
2025-01-18 10:21:20 +0000

    added the condition

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from minimal_mlops.src.confs.config import load_config
 from minimal_mlops.src.training_pipeline.trainer import training_engine
 from minimal_mlops.src.inference_pipeline.inference import InferenceAPI
+from typing import Dict
+from pathlib import Path
+import os
 import argparse
 
-def main(mode: str):
+def main(mode: str, config: Dict):
     config = load_config()
     if mode == "train":
         inst = training_engine(config=config)
     elif mode == "infer":
-        inst = InferenceAPI(config=config)
-        inst.run()
+        if len(os.listdir(Path(os.path.join(config["path"]["root"], config["path"]["models"])))) == 1:
+            print("Train First, then initiate inference")
+        else:
+            inst = InferenceAPI(config=config)
+            inst.run()
     elif mode == "train_with_infer":
         inst = training_engine(config=config)
         inst1 = InferenceAPI(config=config)
@@ -20,4 +26,4 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("mode", choices=["train", "infer", "train_with_infer"])
     args = parser.parse_args()
-    main(mode=args.mode)
+    main(mode=args.mode, config=load_config())
